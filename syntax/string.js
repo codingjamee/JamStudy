3b18734ec267913fb12e5e6dc84095f5762f55b1
codingjame <codingjamee@gmail.com>
2022-02-16 23:59:24 +0900

    문자열세기

---
+++ syntax/string.js
@@ -0,0 +1,4 @@
+console.log(1+1);
+console.log('1'+'1');
+
+console.log('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur nulla nec dui dictum sagittis. Donec enim est, bibendum a mattis at, tempus nec felis. Nunc cursus massa eget varius bibendum. Nullam mollis, urna in fermentum pretium, ante justo tincidunt arcu, sed convallis nulla orci ultrices lectus. Fusce cursus mi odio. Sed eros justo, pulvinar in consequat et, interdum sed orci. Nam eros ex, vehicula id massa eget, sagittis tincidunt leo. Mauris facilisis, nisi vitae tristique dictum, ipsum justo eleifend tellus, vel molestie purus dolor quis lacus. Curabitur venenatis interdum imperdiet. Donec pellentesque in nisi eget ultrices. Nam iaculis pulvinar.'.length);
